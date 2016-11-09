dace23d2330d40bb3e326f60d0b435700535d906
Ferrandiz93 <ferrandiz1993@gmail.com>
2016-11-09 18:19:15 +0100

    prefab moneda
    la moneda se lanza en una direccion aleatoria cada vez que se inicia

---
+++ Assets/Scripts/moneda.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moneda : MonoBehaviour
+{
+	private Rigidbody2D rb;
+
+	void Start () {
+		Destroy (gameObject, 3);
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (new Vector2 (Random.Range(-100,100),100));
+	}
+    // función para controlar cuando un objeto entra en el trigger
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Collider2D col hace referencia al objeto que ha entrado
+        Debug.Log("Alguien me ha tocado");
+        Destroy(gameObject);
+    }
+
+    // función para controlar cuando un objeto sale del trigger
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Debug.Log("Han dejado de tocarme");
+    }
+    // función para controlar cuando un objeto permanece en el trigger
+    void OnTriggerStay2D(Collider2D col)
+    {
+        Debug.Log("Me están tocando, me siento incomodo");
+    }
+}
